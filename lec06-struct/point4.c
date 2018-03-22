cc4936d2633a716d11303840fb50a659947e51a3
ZHAO Jing <zj@ahstu.cc>
2018-03-22 15:39:53 +0800

    上传文件至 'lec06-struct'

---
+++ lec06-struct/point4.c
@@ -0,0 +1,25 @@
+///(*p).x  <=> p->x
+/// using typedef
+typedef struct{
+    int x;
+    int y;
+} POINT;
+
+void input(POINT* p){
+
+ scanf("%d%d",&p->x,&p->y);///right neat
+}
+void print(POINT* p){
+    printf("%d %d\n",p->x,p->y);
+}
+
+
+int main(){
+    POINT p = {0,4},q= {3,4},r= {0,0};
+    printf("sizeof(struct Point)=%d\n",sizeof(POINT));
+    printf("sizeof(struct Point*)=%d\n",sizeof(POINT*));
+    input(&p);
+    print(&p);
+
+
+}
