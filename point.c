6f24645a3ae0bb3b82a973d2b3572a063910d8b3
ZHAO Jing <zj@ahstu.cc>
2018-03-22 15:39:25 +0800

    上传文件至 ''

---
+++ point.c
@@ -0,0 +1,17 @@
+
+struct Point{
+    int x;
+    int y;
+};
+
+
+
+int main()
+{
+    struct Point p;
+    scanf("%d%d",&p.x,&p.y);// . ->
+    printf("%d %d\n",p.x,p.y);
+    struct Point q,r;
+
+
+}
---
+++ point2.c
@@ -0,0 +1,27 @@
+
+struct Point
+{
+    int x;
+    int y;
+};
+void input(struct Point p)///wrong  pass by value
+{
+    printf("In function input: &p=%x\n",&p);
+    scanf("%d%d",&p.x,&p.y);
+}
+void print(struct Point p)
+{
+    printf("%d %d\n",p.x,p.y);
+}
+
+
+int main()
+{
+    struct Point p= {0,4},q= {3,4},r= {0,0};
+    printf("In function main: &p=%x\n",&p);
+    input(p);
+    print(p);
+
+
+
+}
