1568351ffdb2e95f93ee4da753668c53a4913032
ZHAO Jing <ceo@webturing.com>
2018-04-26 16:02:42 +0800

    Create pre.c

---
+++ lec14/pre.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d\n",3^3);
+    printf("%lld\n",100*100*100*100*100);
+    printf("%lld\n",100*100*100*100*100*1LL);
+    printf("%lld\n",1LL*100*100*100*100*100);
+}
