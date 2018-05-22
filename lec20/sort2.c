c106476581de56ca9b1a3f37d2c5c22c5d71db40
webturing <zj@webturing.com>
2018-05-23 07:07:05 +0800

    week12

--- lec20/sort2.c
+++ lec20/sort2.c
@@ -1,21 +1,17 @@
-typedef struct{
-    char *str;
-    int freq;
+typedef struct {
+  char *str;
+  int freq;
 
-}Node;
+} Node;
 
-int cmp(const void*a,const void*b){
-    Node *na=(Node*)(a);
-    Node *nb=(Node*)(b);
-    return strcmp(na->str,nb->str);
-
+int cmp(const void *a, const void *b) {
+  Node *na = (Node *)(a);
+  Node *nb = (Node *)(b);
+  return strcmp(na->str, nb->str);
 }
-int main()
-{
-Node a={"zero",1};
-Node b={"is",1};
-int dif=cmp(&a,&b);
-printf("%d\n",dif);
-
-
+int main() {
+  Node a = {"zero", 1};
+  Node b = {"is", 1};
+  int dif = cmp(&a, &b);
+  printf("%d\n", dif);
 }
--- lec20/strSort2.c
+++ lec20/strSort2.c
@@ -1,36 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int mycmp1(const void*a,const void*b){//×ÖµäÔöĞò
-    char **sa=(char**)a;
-    char **sb=(char**)b;
-    return strcmp(*sa,*sb);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int mycmp1(const void *a, const void *b) {  //×ÖµäÔöĞò
+  char **sa = (char **)a;
+  char **sb = (char **)b;
+  return strcmp(*sa, *sb);
 }
-int mycmp2(const void*a,const void*b){//×Öµä½µĞò
-    char **sa=(char**)a;
-    char **sb=(char**)b;
-    return -strcmp(*sa,*sb);
+int mycmp2(const void *a, const void *b) {  //×Öµä½µĞò
+  char **sa = (char **)a;
+  char **sb = (char **)b;
+  return -strcmp(*sa, *sb);
 }
-int mycmp3(const void*a,const void*b){//³¤¶ÈÔöĞò
-    char **sa=(char**)a;
-    char **sb=(char**)b;
-    return strlen(*sa)-strlen(*sb);
+int mycmp3(const void *a, const void *b) {  //³¤¶ÈÔöĞò
+  char **sa = (char **)a;
+  char **sb = (char **)b;
+  return strlen(*sa) - strlen(*sb);
 }
-int mycmp4(const void*a,const void*b){
-    char **sa=(char**)a;
-    char **sb=(char**)b;
-    int difLen= strlen(*sa)-strlen(*sb);
-    if(difLen)return difLen;
-    return mycmp1(a,b);//
+int mycmp4(const void *a, const void *b) {
+  char **sa = (char **)a;
+  char **sb = (char **)b;
+  int difLen = strlen(*sa) - strlen(*sb);
+  if (difLen) return difLen;
+  return mycmp1(a, b);  //
+}
+int main() {
+  char *s[10] = {"zero", "one", "two",   "three", "four",
+                 "five", "six", "seven", "eight", "nine"};
+  qsort(s, 10, sizeof(s[0]), mycmp4);
+  int i;
+  for (i = 0; i < 10; i++) puts(s[i]);
 
-}
-int main()
-{
-    char *s[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
-    qsort(s,10,sizeof(s[0]),mycmp4);
-    int i;
-    for(i=0;i<10;i++)
-        puts(s[i]);
-
-    return 0;
+  return 0;
 }
